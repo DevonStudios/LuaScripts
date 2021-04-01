4800feab8ee390b4aae247a832f72ef9f37c376e
Real96 <chicco.gaeta@gmail.com>
2021-04-01 14:22:32 +0200

    [Dolphin] Refactoring

--- Gen 3/Dolphin/Colosseum.lua
+++ Gen 3/Dolphin/Colosseum.lua
@@ -33,14 +33,14 @@ local pokemon = {
  "CLAMPERL", "HUNTAIL", "GOREBYSS", "RELICANTH", "LUVDISC", "BAGON", "SHELGON", "SALAMENCE", "BELDUM", "METANG", "METAGROSS", "REGIROCK", "REGICE",
  "REGISTEEL", "LATIAS", "LATIOS", "KYOGRE", "GROUDON", "RAYQUAZA", "JIRACHI", "DEOXYS"}
 
-local naturename = {
+local natureName = {
  "Hardy", "Lonely", "Brave", "Adamant", "Naughty",
  "Bold", "Docile", "Relaxed", "Impish", "Lax",
  "Timid", "Hasty", "Serious", "Jolly", "Naive",
  "Modest", "Mild", "Quiet", "Bashful", "Rash",
  "Calm", "Gentle", "Sassy", "Careful", "Quirky"}
 
-local typeorder = {
+local typeOrder = {
  "Fighting", "Flying", "Poison", "Ground",
  "Rock", "Bug", "Ghost", "Steel",
  "Fire", "Water", "Grass", "Electric",
@@ -97,192 +97,191 @@ local catchRate = {
  15, 3, 3, 3, 3, 3, 5, 5, 3, 3, 3}
 
 function next(s)
-	local a = 0x3 * (s % 65536) + (s >> 16) * 0x43FD
-	local b = 0x43FD * (s % 65536) + (a % 65536) * 65536 + 0x269EC3
-	local c = b % 4294967296
-	return c
+ local a = 0x3 * (s % 65536) + (s >> 16) * 0x43FD
+ local b = 0x43FD * (s % 65536) + (a % 65536) * 65536 + 0x269EC3
+ local c = b % 4294967296
+ return c
 end
 
 function back(s)
-	local a = 0xB9B3 * (s % 65536) + (s >> 16) * 0x3155
-	local b = 0x3155 * (s % 65536) + (a % 65536) * 65536 + 0xA170F641
-	local c = b % 4294967296
-	return c
+ local a = 0xB9B3 * (s % 65536) + (s >> 16) * 0x3155
+ local b = 0x3155 * (s % 65536) + (a % 65536) * 65536 + 0xA170F641
+ local c = b % 4294967296
+ return c
 end
 
 function setPadding(maxLength, goodSpacing, stringVar)
-	spaces = ""
-	if string.len(stringVar) <= maxLength then
-		padding = maxLength - string.len(stringVar)
-		for i = 0, padding + goodSpacing do
-			spaces = spaces..string.format(" ")
-		end
-	end
-	return spaces
+ spaces = ""
+ if string.len(stringVar) <= maxLength then
+  padding = maxLength - string.len(stringVar)
+  for i = 0, padding + goodSpacing do
+   spaces = spaces..string.format(" ")
+  end
+ end
+ return spaces
 end
 
 function calcRate(HP, bonusBall, rate)
-	a = math.floor((HP * rate * bonusBall) / (3 * HP))
-	return math.floor(1048560 / math.sqrt(math.sqrt(16711680 / a)))
+ a = math.floor((HP * rate * bonusBall) / (3 * HP))
+ return math.floor(1048560 / math.sqrt(math.sqrt(16711680 / a)))
 end
 
 function setInfo(start, partyStart, boxStart, i)
-	pid = ReadValue32(start)
-	speciesAddr = start - 0x4
-	index = nationalDexTable[ReadValue16(speciesAddr) + 1] + 1
-	species = pokemon[index]
-	species = species..setPadding(10, 5, species)
-	nature = naturename[(pid % 25) + 1]
-	nature = nature..setPadding(7, 9, nature)
-	catchValue = calcRate(ReadValue16(start + 0x86), 1, catchRate[index])
+ pid = ReadValue32(start)
+ speciesAddr = start - 0x4
+ index = nationalDexTable[ReadValue16(speciesAddr) + 1] + 1
+ species = pokemon[index]
+ species = species..setPadding(10, 5, species)
+ nature = natureName[(pid % 25) + 1]
+ nature = nature..setPadding(7, 9, nature)
+ catchValue = calcRate(ReadValue16(start + 0x86), 1, catchRate[index])
 
-	ivsAddr = start + 0xA1
-	hpiv = ReadValue8(ivsAddr)
-	atkiv = ReadValue8(ivsAddr + 0x2)
-	defiv = ReadValue8(ivsAddr + 0x4)
-    spatkiv = ReadValue8(ivsAddr + 0x8)
-	spdefiv = ReadValue8(ivsAddr + 0x6)
-	spdiv = ReadValue8(ivsAddr + 0xA)
+ ivsAddr = start + 0xA1
+ hpiv = ReadValue8(ivsAddr)
+ atkiv = ReadValue8(ivsAddr + 0x2)
+ defiv = ReadValue8(ivsAddr + 0x4)
+ spatkiv = ReadValue8(ivsAddr + 0x8)
+ spdefiv = ReadValue8(ivsAddr + 0x6)
+ spdiv = ReadValue8(ivsAddr + 0xA)
 
-	hidpowtype = ((hpiv%2 + 2*(atkiv%2) + 4*(defiv%2) + 8*(spdiv%2) + 16*(spdefiv%2) + 32*(spatkiv%2))*15)//63
-	hidpowbase = (((hpiv&2)/2 + (atkiv&2) + 2*(defiv&2) + 4*(spdiv&2) + 8*(spdefiv&2) + 16*(spatkiv&2))*40)//63 + 30
+ hidpowtype = ((hpiv%2 + 2*(atkiv%2) + 4*(defiv%2) + 8*(spdiv%2) + 16*(spdefiv%2) + 32*(spatkiv%2))*15) // 63
+ hidpowbase = (((hpiv&2)/2 + (atkiv&2) + 2*(defiv&2) + 4*(spdiv&2) + 8*(spdefiv&2) + 16*(spatkiv&2))*40) // 63 + 30
 
-	partyPid = ReadValue32(partyStart)
-	partySpeciesAddr = partyStart - 0x4
-	partyIndex = nationalDexTable[ReadValue16(partySpeciesAddr) + 1] + 1
-	partySpecies = pokemon[partyIndex]
-	partySpecies = partySpecies..setPadding(10, 5, partySpecies)
-	partyNature = naturename[(partyPid % 25) + 1]
-	partyNature = partyNature..setPadding(7, 9, partyNature)
+ partyPid = ReadValue32(partyStart)
+ partySpeciesAddr = partyStart - 0x4
+ partyIndex = nationalDexTable[ReadValue16(partySpeciesAddr) + 1] + 1
+ partySpecies = pokemon[partyIndex]
+ partySpecies = partySpecies..setPadding(10, 5, partySpecies)
+ partyNature = natureName[(partyPid % 25) + 1]
+ partyNature = partyNature..setPadding(7, 9, partyNature)
 
-	partyIvsAddr = partyStart + 0xA1
-	partyHpiv = ReadValue8(partyIvsAddr)
-	partyAtkiv = ReadValue8(partyIvsAddr + 0x2)
-	partyDefiv = ReadValue8(partyIvsAddr + 0x4)
-    partySpatkiv = ReadValue8(partyIvsAddr + 0x8)
-	partySpdefiv = ReadValue8(partyIvsAddr + 0x6)
-	partySpdiv = ReadValue8(partyIvsAddr + 0xA)
+ partyIvsAddr = partyStart + 0xA1
+ partyHpiv = ReadValue8(partyIvsAddr)
+ partyAtkiv = ReadValue8(partyIvsAddr + 0x2)
+ partyDefiv = ReadValue8(partyIvsAddr + 0x4)
+ partySpatkiv = ReadValue8(partyIvsAddr + 0x8)
+ partySpdefiv = ReadValue8(partyIvsAddr + 0x6)
+ partySpdiv = ReadValue8(partyIvsAddr + 0xA)
 
-	partyHidpowtype = ((partyHpiv%2 + 2*(partyAtkiv%2) + 4*(partyDefiv%2) + 8*(partySpdiv%2) + 16*(partySpdefiv%2) + 32*(partySpatkiv%2))*15) // 63
-	partyHidpowbase = (((partyHpiv&2)/2 + (partyAtkiv&2) + 2*(partyDefiv&2) + 4*(partySpdiv&2) + 8*(partySpdefiv&2) + 16*(partySpatkiv&2))*40) // 63 + 30
+ partyHidpowtype = ((partyHpiv%2 + 2*(partyAtkiv%2) + 4*(partyDefiv%2) + 8*(partySpdiv%2) + 16*(partySpdefiv%2) + 32*(partySpatkiv%2))*15) // 63
+ partyHidpowbase = (((partyHpiv&2)/2 + (partyAtkiv&2) + 2*(partyDefiv&2) + 4*(partySpdiv&2) + 8*(partySpdefiv&2) + 16*(partySpatkiv&2))*40) // 63 + 30
 
-	if i == 0 then
-		boxPid = ReadValue32(boxStart)
-		boxSpeciesAddr = boxStart - 0x4
-		boxIndex = nationalDexTable[ReadValue16(boxSpeciesAddr) + 1] + 1
-		boxSpecies = pokemon[boxIndex]
-		boxNature = naturename[(boxPid % 25)+1]
+ if i == 0 then
+  boxPid = ReadValue32(boxStart)
+  boxSpeciesAddr = boxStart - 0x4
+  boxIndex = nationalDexTable[ReadValue16(boxSpeciesAddr) + 1] + 1
+  boxSpecies = pokemon[boxIndex]
+  boxNature = natureName[(boxPid % 25)+1]
 
-		boxIvsAddr = boxStart + 0xA1
-		boxHpiv = ReadValue8(boxIvsAddr)
-		boxAtkiv = ReadValue8(boxIvsAddr + 0x2)
-		boxDefiv = ReadValue8(boxIvsAddr + 0x4)
-		boxSpatkiv = ReadValue8(boxIvsAddr + 0x8)
-		boxSpdefiv = ReadValue8(boxIvsAddr + 0x6)
-		boxSpdiv = ReadValue8(boxIvsAddr + 0xA)
+  boxIvsAddr = boxStart + 0xA1
+  boxHpiv = ReadValue8(boxIvsAddr)
+  boxAtkiv = ReadValue8(boxIvsAddr + 0x2)
+  boxDefiv = ReadValue8(boxIvsAddr + 0x4)
+  boxSpatkiv = ReadValue8(boxIvsAddr + 0x8)
+  boxSpdefiv = ReadValue8(boxIvsAddr + 0x6)
+  boxSpdiv = ReadValue8(boxIvsAddr + 0xA)
 
-		boxHidpowtype = ((boxHpiv%2 + 2*(boxAtkiv%2) + 4*(boxDefiv%2) + 8*(boxSpdiv%2) + 16*(boxSpdefiv%2) + 32*(boxSpatkiv%2))*15) // 63
-		boxHidpowbase = (((boxHpiv&2)/2 + (boxAtkiv&2) + 2*(boxDefiv&2) + 4*(boxSpdiv&2) + 8*(boxSpdefiv&2) + 16*(boxSpatkiv&2))*40) // 63 + 30
+  boxHidpowtype = ((boxHpiv%2 + 2*(boxAtkiv%2) + 4*(boxDefiv%2) + 8*(boxSpdiv%2) + 16*(boxSpdefiv%2) + 32*(boxSpatkiv%2))*15) // 63
+  boxHidpowbase = (((boxHpiv&2)/2 + (boxAtkiv&2) + 2*(boxDefiv&2) + 4*(boxSpdiv&2) + 8*(boxSpdefiv&2) + 16*(boxSpatkiv&2))*40) // 63 + 30
 
-		speciesText = string.format("Species: ")..species..string.format("Species: ")..partySpecies..string.format("Species: ")..boxSpecies
-		PIDs = string.format("\nPID: %08X", pid)..string.format("            PID: %08X", partyPid)..string.format("            PID: %08X", boxPid)
-		naturesText = string.format("\nNature: ")..nature..string.format("Nature: ")..partyNature..string.format("Nature: ")..boxNature
-		ivs = string.format("\nIVs: %02d", hpiv)..string.format("/%02d", atkiv)..string.format("/%02d", defiv)..string.format("/%02d", spdefiv)..string.format("/%02d", spatkiv)..string.format("/%02d", spdiv)..string.format("   IVs: %02d", partyHpiv)..string.format("/%02d", partyAtkiv)..string.format("/%02d", partyDefiv)..string.format("/%02d", partySpdefiv)..string.format("/%02d", partySpatkiv)..string.format("/%02d", partySpdiv)..string.format("   IVs: %02d", boxHpiv)..string.format("/%02d", boxAtkiv)..string.format("/%02d", boxDefiv)..string.format("/%02d", boxSpdefiv)..string.format("/%02d", boxSpatkiv)..string.format("/%02d", boxSpdiv)
-		hiddenPower = "\nHP: "..typeorder[hidpowtype + 1].." "..string.format("%02d", hidpowbase)..setPadding(11, 9, typeorder[hidpowtype + 1].." "..string.format("%02d", hidpowbase)).."HP: "..typeorder[partyHidpowtype + 1].." "..string.format("%02d", partyHidpowbase)..setPadding(11, 9, typeorder[partyHidpowtype + 1].." "..string.format("%02d", partyHidpowbase)).."HP: "..typeorder[boxHidpowtype + 1].." "..string.format("%02d", boxHidpowbase).."\n"
-		catchrngText = string.format("Catch Rate Value: %.0f", catchValue)
-		infoText = speciesText..PIDs..naturesText..ivs..hiddenPower..catchrngText
-	else
-		speciesText = string.format("Species: ")..species..string.format("Species: ")..partySpecies
-		PIDs = string.format("\nPID: %08X", pid)..string.format("            PID: %08X", partyPid)
-		naturesText = string.format("\nNature: ")..nature..string.format("Nature: ")..partyNature
-		ivs = string.format("\nIVs: %02d", hpiv)..string.format("/%02d", atkiv)..string.format("/%02d", defiv)..string.format("/%02d", spdefiv)..string.format("/%02d", spatkiv)..string.format("/%02d", spdiv)..string.format("   IVs: %02d", partyHpiv)..string.format("/%02d", partyAtkiv)..string.format("/%02d", partyDefiv)..string.format("/%02d", partySpdefiv)..string.format("/%02d", partySpatkiv)..string.format("/%02d", partySpdiv)
-		hiddenPower = "\nHP: "..typeorder[hidpowtype + 1].." "..string.format("%02d", hidpowbase)..setPadding(11, 9, typeorder[hidpowtype + 1].." "..string.format("%02d", hidpowbase)).."HP: "..typeorder[partyHidpowtype + 1].." "..string.format("%02d", partyHidpowbase).."\n"
-		catchrngText = string.format("Catch Rate Value: %.0f", catchValue)
-		infoText = speciesText..PIDs..naturesText..ivs..hiddenPower..catchrngText
-	end
-	return infoText
+  speciesText = string.format("Species: %sSpecies: %sSpecies: %s", species, partySpecies, boxSpecies)
+  PIDs = string.format("\nPID: %08X            PID: %08X            PID: %08X", pid, partyPid, boxPid)
+  naturesText = string.format("\nNature: %sNature: %sNature: %s", nature, partyNature, boxNature)
+  ivs = string.format("\nIVs: %02d/%02d/%02d/%02d/%02d/%02d   IVs: %02d/%02d/%02d/%02d/%02d/%02d   IVs: %02d/%02d/%02d/%02d/%02d/%02d", hpiv, atkiv, defiv, spdefiv, spatkiv, spdiv, partyHpiv, partyAtkiv, partyDefiv, partySpdefiv, partySpatkiv, partySpdiv, boxHpiv, boxAtkiv, boxDefiv, boxSpdefiv, boxSpatkiv, boxSpdiv)
+  hiddenPower = string.format("\nHP: %s %02d", typeOrder[hidpowtype + 1], hidpowbase)..setPadding(11, 9, string.format("%s %02d", typeOrder[hidpowtype + 1], hidpowbase))..string.format("HP: %s %02d", typeOrder[partyHidpowtype + 1], partyHidpowbase)..setPadding(11, 9, string.format("%s %02d", typeOrder[partyHidpowtype + 1], partyHidpowbase))..string.format("HP: %s %02d", typeOrder[boxHidpowtype + 1], boxHidpowbase).."\n"
+  catchRngText = string.format("Catch Rate Value: %.0f", catchValue)
+  infoText = speciesText..PIDs..naturesText..ivs..hiddenPower..catchRngText
+ else
+  speciesText = string.format("Species: %sSpecies: %s", species, partySpecies)
+  PIDs = string.format("\nPID: %08X            PID: %08X", pid, partyPid)
+  naturesText = string.format("\nNature: %sNature: %s", nature, partyNature)
+  ivs = string.format("\nIVs: %02d/%02d/%02d/%02d/%02d/%02d   IVs: %02d/%02d/%02d/%02d/%02d/%02d", hpiv, atkiv, defiv, spdefiv, spatkiv, spdiv, partyHpiv, partyAtkiv, partyDefiv, partySpdefiv, partySpatkiv, partySpdiv)
+  hiddenPower = string.format("\nHP: %s %02d", typeOrder[hidpowtype + 1], hidpowbase)..setPadding(11, 9, string.format("%s %02d", typeOrder[hidpowtype + 1], hidpowbase))..string.format("HP: %s %02d", typeOrder[partyHidpowtype + 1], partyHidpowbase).."\n"
+  catchRngText = string.format("Catch Rate Value: %.0f", catchValue)
+  infoText = speciesText..PIDs..naturesText..ivs..hiddenPower..catchRngText
+ end
+ return infoText
 end
 
 function claclFrameJump()
-	calibrationFrame = 0
-	if tempSeed ~= currSeed then
-		tempSeed2 = tempSeed
-		while tempSeed ~= currSeed and tempSeed2 ~= currSeed do
-			tempSeed = next(tempSeed)
-			tempSeed2 = back(tempSeed2)
-			calibrationFrame = calibrationFrame + 1
-		end
+ calibrationFrame = 0
+ if tempSeed ~= currSeed then
+  tempSeed2 = tempSeed
+  while tempSeed ~= currSeed and tempSeed2 ~= currSeed do
+   tempSeed = next(tempSeed)
+   tempSeed2 = back(tempSeed2)
+   calibrationFrame = calibrationFrame + 1
+  end
 
-		if tempSeed2 == currSeed and tempSeed2 ~= tempSeed then
-			calibrationFrame = (-1) * calibrationFrame
-			tempSeed = tempSeed2
-		end
-	end
-	return calibrationFrame
+  if tempSeed2 == currSeed and tempSeed2 ~= tempSeed then
+   calibrationFrame = (-1) * calibrationFrame
+   tempSeed = tempSeed2
+  end
+ end
+ return calibrationFrame
 end
 
 function onScriptStart()
-	if ReadValue32(0x0) == 0x4743364A then -- J
-		IDsOff = 0
-		startOff = 0
-		boxOff = 0
-		seedOff = 0
-	elseif ReadValue32(0x0) == 0x47433645 then -- U
-		IDsOff = 0x91D44
-		startOff = 0x14920
-		boxOff = 0x13930
-		seedOff = 0x14930
-	else
-		IDsOff = 0x1AE278
-		startOff = 0x61DB8
-		boxOff = 0x60DB0
-		seedOff = 0x61DD0
-	end
+ if ReadValue32(0x0) == 0x4743364A then -- J
+  IDsOff = 0
+  startOff = 0
+  boxOff = 0
+  seedOff = 0
+ elseif ReadValue32(0x0) == 0x47433645 then -- U
+  IDsOff = 0x91D44
+  startOff = 0x14920
+  boxOff = 0x13930
+  seedOff = 0x14930
+ else
+  IDsOff = 0x1AE278
+  startOff = 0x61DB8
+  boxOff = 0x60DB0
+  seedOff = 0x61DD0
+ end
 
-	start = 0x45E750 + startOff
-	seedAddrs = 0x464360 + seedOff
-	boxStart = 0x395CBC + boxOff
-	info = "\n\n"
-	TID = 0
-	SID = 0
-	initialSeed = 0
-	currSeed = 0
-	tempSeed = 0
-	tempSeed2 = 0
-	frame = 0
+ start = 0x45E750 + startOff
+ seedAddr = 0x464360 + seedOff
+ boxStart = 0x395CBC + boxOff
+ info = "\n\n"
+ TID = 0
+ SID = 0
+ initSeed = 0
+ currSeed = 0
+ tempSeed = 0
+ frame = 0
 end
 
 function onScriptUpdate()
-	IDsPointer = ReadValue32(0x75E088 + IDsOff)
+ IDsPointer = ReadValue32(0x75E088 + IDsOff)
 
-	if IDsPointer ~= 0 then
-		TID = ReadValue16(IDsPointer + 0x7FFE42E2)
-		SID = ReadValue16(IDsPointer + 0x7FFE42E0)
-		partyStart = IDsPointer + 0x7FFE42E8
-		info = string.format("\n\nOpponent")..string.format("                 Party")..string.format("                    Box\n\n")
+ if IDsPointer ~= 0 then
+  TID = ReadValue16(IDsPointer + 0x7FFE42E2)
+  SID = ReadValue16(IDsPointer + 0x7FFE42E0)
+  partyStart = IDsPointer + 0x7FFE42E8
+  info = string.format("\n\nOpponent                 Party                    Box\n\n")
 
-		for i = 0, 5 do
-			info = info..setInfo(start + (0x138 * i), partyStart + (0x138 * i), boxStart, i).."\n\n"
-		end
-	end
+  for i = 0, 5 do
+   info = info..setInfo(start + (0x138 * i), partyStart + (0x138 * i), boxStart, i).."\n\n"
+  end
+ end
 
-	if ReadValue32(seedAddrs) > 0xFFFF and initialSeed == 0 then
-		initialSeed = ReadValue32(seedAddrs)
-		tempSeed = initialSeed
-		frame = 0
-	end
+ if ReadValue32(seedAddr) > 0xFFFF and initSeed == 0 then
+  initSeed = ReadValue32(seedAddr)
+  tempSeed = initSeed
+  frame = 0
+ end
 
-	currSeed = ReadValue32(seedAddrs)
-	frame = frame + claclFrameJump()
+ currSeed = ReadValue32(seedAddr)
+ frame = frame + claclFrameJump()
 
-	RNGInfo = string.format("Initial Seed: %08X", initialSeed)..string.format("\nCurrent Seed: %08X", currSeed)..string.format("\nFrame: %d", frame)
-	IDs = string.format("\n\n\nTID: %05d", TID)..string.format("\nSID: %05d", SID)
-	text = RNGInfo..info..IDs
-	SetScreenText(text)
+ RNGInfo = string.format("Initial Seed: %08X\nCurrent Seed: %08X\nFrame: %d", initSeed, currSeed, frame)
+ IDs = string.format("\n\n\nTID: %05d\nSID: %05d", TID, SID)
+ text = RNGInfo..info..IDs
+ SetScreenText(text)
 end
 
 function onScriptCancel()
-	SetScreenText("")
+ SetScreenText("")
 end
 
 function onStateLoaded()
