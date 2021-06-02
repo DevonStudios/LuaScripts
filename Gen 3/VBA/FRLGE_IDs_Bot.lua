b11eac8e6827089cf13db8ea8ed11d6635da57cd
Real96 <chicco.gaeta@gmail.com>
2021-06-02 14:23:07 +0200

    [FRLGE_Bot] Changed spaces

--- Gen 3/VBA/FRLGE_IDs_Bot.lua
+++ Gen 3/VBA/FRLGE_IDs_Bot.lua
@@ -5,16 +5,16 @@ local notFound = true
 local targetTID = {}  -- Input here the target TIDs
 
 function writeCheck()
-	seedWritten = true
+ seedWritten = true
 end
 
 function isNotFound()
-	for i = 1, table.getn(targetTID) do
-		if tid == targetTID[i] then
-			return false
-		end
-	end
-	return true
+ for i = 1, table.getn(targetTID) do
+  if tid == targetTID[i] then
+   return false
+  end
+ end
+ return true
 end
 
 memory.registerwrite(0x2020000, writeCheck)
@@ -22,25 +22,26 @@ memory.registerwrite(0x2020000, writeCheck)
 print("Searching...")
 
 while notFound do
-	savestate.save(state)
-	joypad.set(1, {A=true})
-	i = 0
-	while seedWritten == false and i < 36 do
-		emu.frameadvance()
-		i = i + 1
-	end
-	if seedWritten then
-		tid = memory.readword(0x2020000)
-		notFound = isNotFound()
-	end
-	if notFound then
-		seedWritten = false
-		savestate.load(state)
-		emu.frameadvance()
-	end
+ savestate.save(state)
+ joypad.set(1, {A=true})
+ i = 0
+ while seedWritten == false and i < 36 do
+  emu.frameadvance()
+  i = i + 1
+ end
+ if seedWritten then
+  tid = memory.readword(0x2020000)
+  notFound = isNotFound()
+ end
+ if notFound then
+  seedWritten = false
+  savestate.load(state)
+  emu.frameadvance()
+ end
 end
 
 print()
 print("Found!")
 print(string.format("TID: %05d", tid))
+
 emu.pause()
