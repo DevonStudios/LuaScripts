e162bcc5e7af129256dcb5161cf230b04921891b
Real96 <chicco.gaeta@gmail.com>
2021-06-02 14:11:29 +0200

    [FRLG_Side VBA] Crit Hit script refactoring and added multilang support

--- Side Beta/Gen3/FRLG_Critical_Hit_RNG.lua
+++ Side Beta/Gen3/FRLG_Critical_Hit_RNG.lua
@@ -1,118 +1,115 @@
-rshift=bit.rshift
-lshift=bit.lshift
+mdword = memory.readdwordunsigned
+mbyte = memory.readbyte
+rshift = bit.rshift
+band = bit.band
 
---local gamelang = memory.readbyte(0x080000AF)
+local gameLang = mbyte(0x080000AF)
 
---[[if gamelang == 0x4A then 	 --J = 4A
- off = 0
- frameoff = 0
-elseif gamelang == 0x45 then --U = 45
- off = 0xD0
- frameoff = 0x90
-else					 	 --E all the rest
- off = 0xE0
- frameoff = 0x90
-end ]]
+if gameLang == 0x4A then -- J = 0x4A
+ currSeedAddr = 0x03004FA0
+elseif gameLang == 0x45 then -- U = 0x45
+ currSeedAddr = 0x3005000
+else -- E all the rest
+ currSeedAddr = 0x03004F50
+end
 
-local table={}
+local table = {}
 local delay = 0
-local crithit = 1
-local iscritical = 1
+local critHit = 1
+local seedToCheck = 1
 local off = 1
-local check = 0
- 
-function next(s)	
- local a=0x41C6*(s%65536)+rshift(s,16)*0x4E6D
- local b=0x4E6D*(s%65536)+(a%65536)*65536+0x6073
- local c=b%4294967296
+local reloadState = false
+
+function next(s)
+ local a = 0x41C6 * (s % 65536) + rshift(s, 16) * 0x4E6D
+ local b = 0x4E6D * (s % 65536) + (a % 65536) * 65536 + 0x6073
+ local c = b % 4294967296
  return c
 end
 
-function calchit(currseed)
- local t = currseed
- return rshift(t,16)-lshift(rshift(rshift(t,16), 4), 4)
-end
-
-function searchit(startingseed,delayfound)
- local t = startingseed
- for i=1,delayfound do
+function searcHit(startingSeed, delayFound) -- Find next seed that is going to be checked
+ local t = startingSeed
+ for i = 1, delayFound do
   t = next(t)
  end
- return calchit(t)
+ return t
+end
+
+function isCritHit(startingSeed, delayFound) -- Critical hit check
+ hitSeed = searcHit(startingSeed, delayFound)
+ return band(rshift(hitSeed, 16), 0xF) == 0
 end
 
 print("New Order of Breeding x Real.96")
 
 while true do
+ currSeed = mdword(currSeedAddr)
+ frameDifference = 0
 
- seed = memory.readdwordunsigned(0x03004F50)
- framedifference = 0
- 
- table=joypad.get(1)
+ table = joypad.get(1)
  if table.start then
-  startsearchdelay = 1
-  framedelay = 0
+  startDelayResearch = 1
+  frameDelay = 0
   skips2 = 0
-  seed2 = memory.readdword(0x03004F50)
-  j = 0
+  seed2 = currSeed
+  delayCounter = 0
   delay = 0
   state = savestate.create()
   savestate.save(state)
  end
- 
- if startsearchdelay == 1 then  
-  if j <= 150 and delay == 0 then
+
+ if startDelayResearch == 1 then -- Search critical hit delay
+  if delayCounter <= 150 and delay == 0 then
    skips = 0
-   seed3 = memory.readdword(0x03004F50)
-    
+   seed3 = currSeed
+
   if seed3 ~= seed2 then
    while seed3 ~= seed2 do
     seed2 = next(seed2)
     skips = skips + 1
     end
    end
-    
-   framedelay = framedelay + skips
-    
-   print(j,string.format("%08X",seed3), framedelay, "+"..skips)
-   
-   if skips > skips2 and framedelay > 60 then
-    delay = framedelay
+
+   frameDelay = frameDelay + skips
+
+   print(delayCounter, string.format("%08X", seed3), frameDelay, "+"..skips)
+
+   if skips > skips2 and frameDelay > 60 then
+    delay = frameDelay
     print("Delay: ", delay)
-	startsearchdelay = 0
+    startDelayResearch = 0
+    reloadState = true
    end
-   
+
    skips2 = skips
    seed2 = seed3
-   j = j + 1
-   check = 1
+   delayCounter = delayCounter + 1
   end
-  if j == 150 then
-   startsearchdelay = 0
+  if delayCounter == 150 then
+   startDelayResearch = 0
   end
  end
- 
+
  if delay ~= 0 then
-  iscritical = seed
-  off = skips2 - 1
-  while searchit(iscritical,delay) ~= 0 or framedifference % (skips2 - 1) ~= 0 do 
-   iscritical = next(iscritical)
-   framedifference = framedifference + 1
-  end
-  if (check == 1) then
+  if reloadState then
    savestate.load(state)
    emu.pause()
-   check = 0
+   reloadState = false
+  end
+  seedToCheck = currSeed
+  off = skips2 - 1
+  while not isCritHit(seedToCheck, delay) or frameDifference % off ~= 0 do
+   seedToCheck = next(seedToCheck)
+   frameDifference = frameDifference + 1
   end
  end
- 
- if framedifference == 0 then
-  gui.text(0,115, "Critical Hit")
+
+ if frameDifference == 0 then
+  gui.text(0, 115, "Critical Hit")
  else
-  gui.text(0,115, "Normal Hit")
+  gui.text(0, 115, "Normal Hit")
  end
- gui.text(0,125,framedifference / off)
+ gui.text(0, 125, frameDifference / off)
 
  emu.frameadvance()
-
 end
